fe23392d9f7c96e30ba3b5e3dad6e7fd34aa0b71
zain <ahmedzain958@gmail.com>
2025-09-03 20:36:30 +0300

    before modal sheet design and user input

---
+++ android/app/src/main/kotlin/com/example/expense_tracker_maxemallian_course/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.expense_tracker_maxemallian_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:expense_tracker_maxemallian_course/widgets/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(useMaterial3: true), home: Expenses()));
+}
